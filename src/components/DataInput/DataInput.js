dd9417c60d9ac476d2163cbb6c2a484cf9bdf4c3
NedDinev <ned.dinev@gmail.com>
2023-05-10 17:39:23 +0300

    Style components

--- src/components/DataInput/DataInput.js
+++ src/components/DataInput/DataInput.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Papa from "papaparse";
 import EmployeesTable from "../EmployeesTable/EmployeesTable";
+import { Container, Row, Col, Form, Card } from "react-bootstrap";
 
 export default function DataInput() {
   const [data, setData] = useState([]);
@@ -17,8 +18,54 @@ export default function DataInput() {
 
   return (
     <>
-      <input type="file" accept=".csv" onChange={handleFileUpload} />
-      {data.length && <EmployeesTable data={data} />}
+      <Container style={styles.container}>
+        <Row style={styles.row}>
+          <Col style={styles.col}>
+            <Card>
+              <Card.Header>
+                <h3>Pair of employees who have worked together</h3>
+              </Card.Header>
+              <Card.Body>
+                <Card.Text>
+                  Import file to see all employees who have worked together on
+                  common projects for the longest period of time
+                </Card.Text>
+                <Form style={styles.form}>
+                  <Form.Group>
+                    <Form.Control
+                      id="exampleFormControlFile1"
+                      label="Choose a CSV file to upload"
+                      type="file"
+                      accept=".csv"
+                      onChange={handleFileUpload}
+                    />
+                  </Form.Group>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        {!!data.length && <EmployeesTable data={data} />}
+      </Container>
     </>
   );
 }
+
+const styles = {
+  container: {
+    width: "80%",
+  },
+  row: {
+    marginTop: "50px",
+    marginBottom: "50px",
+    display: "flex",
+    justifyContent: "center",
+  },
+  col: {
+    textAlign: "center",
+  },
+
+  form: {
+    margin: "auto",
+  },
+};
--- src/components/EmployeesTable/EmployeesTable.js
+++ src/components/EmployeesTable/EmployeesTable.js
@@ -1,59 +1,32 @@
 import { filterEmployees } from "../../utils/filterEmployees";
 
+import Table from "react-bootstrap/Table";
+
 export default function EmployeesTable(props) {
   const { data } = props;
-
+  console.log(data);
   const filteredData = filterEmployees(data);
 
   return (
-    <>
-      <h1>All employees</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>EmpID</th>
-            <th>ProjectID</th>
-            <th>DateFrom</th>
-            <th>DateTo</th>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Employee ID #1</th>
+          <th>Employee ID #2</th>
+          <th>Project ID</th>
+          <th>Days worked</th>
+        </tr>
+      </thead>
+      <tbody>
+        {filteredData.map((row, index) => (
+          <tr key={index}>
+            <td>{row.firstEmployee}</td>
+            <td>{row.secondEmployee}</td>
+            <td>{row.projectID}</td>
+            <td>{row.daysWorked}</td>
           </tr>
-        </thead>
-        <tbody>
-          {data.map((row, index) => (
-            <tr key={index}>
-              <td>{row.EmpID}</td>
-              <td>{row.ProjectID}</td>
-              <td>{row.DateFrom}</td>
-              <td>{row.DateTo}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <h1>
-        Employees who have worked together on common projects for the longest
-        period of time
-      </h1>
-
-      <table>
-        <thead>
-          <tr>
-            <th>Employee ID #1</th>
-            <th>Employee ID #2</th>
-            <th>Project ID</th>
-            <th>Days worked</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredData.map((row, index) => (
-            <tr key={index}>
-              <td>{row.firstEmployee}</td>
-              <td>{row.secondEmployee}</td>
-              <td>{row.projectID}</td>
-              <td>{row.daysWorked}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+        ))}
+      </tbody>
+    </Table>
   );
 }
